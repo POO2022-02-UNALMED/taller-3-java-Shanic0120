a75073259e0a09d7d4244f5dd34944db26d71925
Shanic0120 <101580943+Shanic0120@users.noreply.github.com>
2022-08-24 16:42:00 -0500

    Add files

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,18 @@
+package taller3.televisores;
+
+public class Marca {
+	private String nombre;
+
+	public Marca(String nombre) {
+		this.setNombre(nombre);
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,102 @@
+package taller3.televisores;
+
+public class TV {
+	private Marca marca;
+	private int canal = 1;
+	private int precio = 500;
+	boolean estado;
+	private int volumen = 1;
+	private Control control;
+
+	private static int numTV;
+
+	public TV(Marca marca, boolean estado) {
+		this.setMarca(marca);
+		this.estado = estado;
+	}
+
+	public void volumenUp() {
+		if (canal <= 7 && estado) {
+			canal++;
+		}
+	}
+
+	public void volumenDown() {
+		if (canal >= 0 && estado) {
+			canal--;
+		}
+	}
+
+	public void canalUp() {
+		if (canal <= 120 && estado) {
+			canal++;
+		}
+	}
+
+	public void canalDown() {
+		if (canal >= 1 && estado) {
+			canal--;
+		}
+	}
+
+	public void turnOn() {
+		estado = true;
+	}
+
+	public void turnOff() {
+		estado = false;
+	}
+
+	public boolean getEstado() {
+		return estado;
+	}
+
+	public Marca getMarca() {
+		return marca;
+	}
+
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+
+	public int getCanal() {
+		return canal;
+	}
+
+	public void setCanal(int canal) {
+		this.canal = canal;
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+	public int getVolumen() {
+		return volumen;
+	}
+
+	public void setVolumen(int volumen) {
+		this.volumen = volumen;
+	}
+
+	public Control getControl() {
+		return control;
+	}
+
+	public void setControl(Control control) {
+		this.control = control;
+	}
+
+	public static int getNumTV() {
+		return numTV;
+	}
+
+	public static void setNumTV(int numTV) {
+		TV.numTV = numTV;
+	}
+
+}
